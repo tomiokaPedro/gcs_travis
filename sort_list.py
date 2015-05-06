929d5ccbf435d8efde4891b6f27d162b3f4b4ccd
tomiokaPedro <pedro.tomioka@gmail.com>
2015-05-06 15:41:45 -0300

    add bubbleSort in sort_list.py with tests

--- sort_list.py
+++ sort_list.py
@@ -17,6 +17,13 @@ def sort_list(array):
             
             j+=1
 
-    
-
     return unsorted_array
+
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+    return alist
--- test_sort_list.py
+++ test_sort_list.py
@@ -1,24 +1,28 @@
 import unittest
-from sort_list import sort_list
+from sort_list import sort_list, bubbleSort
 
 class TestSortList(unittest.TestCase):
 
+
+
     def setUp(self):
-        pass
+        self.unsorted_array = [3,5,6,1,2,9,8,7,4]
+        self.expected_array = [1,2,3,4,5,6,7,8,9]
 
     def testSortOrder(self):
 
-        unsorted_array = [3,5,6,1,2,9,8,7,4]
-        expected_array = [1,2,3,4,5,6,7,8,9]
-    
-        sorted_array = sort_list(unsorted_array)
+        sorted_array = sort_list(self.unsorted_array)
 
-        self.assertEqual(expected_array, sorted_array)
+        self.assertEqual(self.expected_array, sorted_array)
 
 
     def tearDown(self):
         pass
 
+    def testBubbleSort(self):
+        sorted_array = bubbleSort(self.unsorted_array)
+
+        self.assertEqual(self.expected_array, sorted_array)
 
 
 if __name__ == "__main__":
